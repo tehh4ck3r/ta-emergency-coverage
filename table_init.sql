f93f6a12ddbe88ad6d153e8cc189f26fe08b8ded
Kush Mahajani <kmahajani@scu.edu>
2017-10-30 11:56:57 -0700

    DBs should be done

--- table_init.sql
+++ table_init.sql
@@ -1,7 +1,7 @@
+DROP TABLE IF EXISTS STUDENTAVAIL;
+DROP TABLE IF EXISTS STUDENTHOURS;
 DROP TABLE IF EXISTS CLASSES;
 DROP TABLE IF EXISTS USERS;
-DROP TABLE IF EXISTS STUDENTAVAIL;
-DROP TABLE IF EXISTS SUTDENTHOURS;
 
 CREATE TABLE USERS(
 	username			VARCHAR(16),
@@ -11,6 +11,8 @@ CREATE TABLE USERS(
 	notify_primary		CHAR(2),
 	notify_secondary	CHAR(2),
 	role				CHAR(4),
+	first				VARCHAR(16),
+	last				VARCHAR(16),
 	PRIMARY KEY (username)
 );
 
@@ -31,5 +33,31 @@ CREATE TABLE CLASSES(
 			REFERENCES USERS (username),
 	CONSTRAINT prof_fkey
 		FOREIGN KEY (professor)
+			REFERENCES USERS (username)
+	-- TODO: Ensure prof has role set as prof
+);
+
+CREATE TABLE STUDENTAVAIL(
+	username		VARCHAR(16),
+	section_id		INT,
+	section_date	DATE,
+	start_time		TIME,
+	avail_start		TIME,
+	avial_end		TIME,
+	PRIMARY KEY (username, section_id, section_date, start_time),
+	CONSTRAINT avail_username_fkey
+		FOREIGN KEY (username)
+			REFERENCES USERS (username),
+	CONSTRAINT sections
+		FOREIGN KEY (section_id, section_date, start_time)
+			REFERENCES CLASSES (section_id, section_date, start_time)
+);
+
+CREATE TABLE STUDENTHOURS(
+	username		VARCHAR(16),
+	hours_worked	INT,
+	hours_scheduled	INT,
+	CONSTRAINT hours_username_fkey
+		FOREIGN KEY (username)
 			REFERENCES USERS (username)
 );
--- test_data.sql
+++ test_data.sql
@@ -1,4 +1,19 @@
-INSERT INTO USERS VALUES ('kmahajani', '123test', 'kmahajani@scu.edu', '1234567890', 'ph', 'em', 'ta');
-INSERT INTO USERS VALUES ('mhao', 'asdf', 'mhao@scu.edu', '1234567890', 'em', 'ph', 'ta');
-INSERT INTO USERS VALUES ('orao', 'securepass', 'orao@scu.edu', '1234567890', 'ph', 'em', 'ta');
-INSERT INTO USERS VALUES ('rdanielson', 'lol', 'rdanielson@scu.edu', '1234567890', 'ph', 'em', 'prof');
+INSERT INTO USERS VALUES ('kmahajani', '123test', 'kmahajani@scu.edu', '1234567890', 'ph', 'em', 'ta', 'Kush', 'Mahajani');
+INSERT INTO USERS VALUES ('mhao', 'asdf', 'mhao@scu.edu', '1234567890', 'em', 'ph', 'ta', 'Mikey', 'Hao');
+INSERT INTO USERS VALUES ('orao', 'securepass', 'orao@scu.edu', '1234567890', 'ph', 'em', 'ta', 'Ojus', 'Rao');
+INSERT INTO USERS VALUES ('rdanielson', 'lol', 'rdanielson@scu.edu', '1234567890', 'ph', 'em', 'prof', 'Ron', 'Danielson');
+
+INSERT INTO CLASSES VALUES ('COEN 10', 0001, '2017-12-01', '010:00:00', '013:15:00', 'F', 2017, 'kmahajani', NULL, 'rdanielson');
+INSERT INTO CLASSES VALUES ('COEN 10', 0001, '2017-12-08', '010:00:00', '013:15:00', 'F', 2017, 'mhao', NULL, 'rdanielson');
+INSERT INTO CLASSES VALUES ('COEN 10', 0001, '2017-12-15', '010:00:00', '013:15:00', 'F', 2017, 'kmahajani', NULL, 'rdanielson');
+INSERT INTO CLASSES VALUES ('COEN 10', 0001, '2017-12-01', '013:30:00', '016:00:00', 'F', 2017, 'orao', NULL, 'rdanielson');
+
+INSERT INTO STUDENTAVAIL VALUES ('kmahajani', 0001, '2017-12-01', '010:00:00', '010:00:00', '013:00:00');
+INSERT INTO STUDENTAVAIL VALUES ('kmahajani', 0001, '2017-12-08', '010:00:00', '010:00:00', '013:15:00');
+INSERT INTO STUDENTAVAIL VALUES ('kmahajani', 0001, '2017-12-15', '010:00:00', '011:00:00', '013:15:00');
+INSERT INTO STUDENTAVAIL VALUES ('orao', 0001, '2017-12-15', '010:00:00', NULL, NULL);
+INSERT INTO STUDENTAVAIL VALUES ('mhao', 0001, '2017-12-15', '010:00:00', '010:00:00', '011:00:00');
+
+INSERT INTO STUDENTHOURS VALUES ('kmahajani', 4, 11);
+INSERT INTO STUDENTHOURS VALUES ('mhao', 0, 15);
+INSERT INTO STUDENTHOURS VALUES ('orao', 1, 14);
