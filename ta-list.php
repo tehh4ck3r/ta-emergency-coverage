e7d1a5c11643955a889d1b3befb10349be497a42
Kush Mahajani <kmahajani@scu.edu>
2017-11-01 13:38:49 -0700

    Sessions!

--- ta-list.php
+++ ta-list.php
@@ -1,5 +1,10 @@
 <?php
 	require('dbconn.php');
+	session_start();
+	if (!isset($_SESSION['username']) && !isset($_SESSION['password'])) {
+		echo($_SESSION['username']);
+		header('location: login.php');
+	}
 ?>
 
 <!DOCTYPE html>
