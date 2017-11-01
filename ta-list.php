fb04335add153c040c91fa41a0908f495ffaf6f5
Kush Mahajani <kmahajani@scu.edu>
2017-11-01 13:12:38 -0700

    Cleaned up TA List

---
+++ ta-list.php
@@ -0,0 +1,27 @@
+<?php
+	require('dbconn.php');
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>TA List</title>
+		<link rel="stylesheet" type="text/css" href="login-style.css">
+	</head>
+	<body>
+		<div class="header">
+			<h2>List of Teaching Assistants</h2>
+		</div>
+		<div class="content">
+			<?php
+				$query = "SELECT first, last from USERS where role = 'ta'";
+				$results = mysqli_query($db, $query, MYSQLI_USE_RESULT);
+				echo('<h2><u>List of TAs</u></h2><br/>');
+				foreach ($results as $i) {
+					echo($i['first'].' '. $i['last']);
+					echo("<br/>");
+				}
+			?>
+		</div>
+	</body>
+</html>
