fdbe93ce2a858067057a35fb03c666f90c014231
Kush Mahajani <kmahajani@scu.edu>
2017-11-02 23:05:41 +0000

    Calendar

--- calendar.php
+++ calendar.php
@@ -2,566 +2,150 @@
 
 ?>
 <!DOCTYPE html>
-
-<html lang="en">
+<html>
 <head>
-<style type="text/css">
-table {
-  font-family: tahoma;
-  width: 100%;
-  border-spacing: 0;
-  border-collapse: separate;
-  table-layout: fixed;
-  margin-bottom: 50px;
-  thead{
-    tr{
-      th{
-        background: @blue;
-        color: @light;
-        padding: 0.5em;
-        overflow: hidden;
-        
-        th:first-child {
-          border-radius: 3px 0 0 0;
-          background-color: black;
-        }
-        th:last-child {
-          border-radius: 0 3px 0 0;
-        }
-        
-        .day {
-          display: block;
-          font-size: 1.2em;
-          border-radius: 50%;
-          width: 30px;
-          height: 30px;
-          margin: 0 auto 5px;
-          padding: 5px;
-          line-height: 1.8;
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/hover.css">
+<link rel="stylesheet" href="css/style.css">
+<script src="js/lib/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="calendar-style.css"> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+<script>
 
-          .day .active {
-            background: @light;
-            color: @blue;
-          }
-        }
-      }
+ $(document).ready(function() {
+  var date = new Date();
+  var d = date.getDate();
+  var m = date.getMonth();
+  var y = date.getFullYear();
+
+  var calendar = $('#calendar').fullCalendar({
+   editable: true,
+   header: {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'month,agendaWeek,agendaDay'
+   },
+
+   events: "events.php",
+
+   eventRender: function(event, element, view) {
+    if (event.allDay === 'true') {
+     event.allDay = true;
+    } else {
+     event.allDay = false;
     }
-  }
-}
-</style>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<table>
-  <thead>
-    <tr>
-      <th></th>
-      <th>
-        <span class="day">Sunday</span>
-      </th>
-      <th>
-        <span class="day">Monday</span>
-      </th>
-      <th>
-        <span class="day">Tuesday</span>
-      </th>
-      <th>
-        <span class="day">Wednesday</span>
-      </th>
-      <th>
-        <span class="day">Thursday</span>
-      </th>
-      <th>
-        <span class="day">Friday</span>
-      </th>
-      <th>
-        <span class="day">Saturday</span>
-      </th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td class="hour" rowspan="4"><span>8:00</span></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td class="hour" rowspan="4"><span>9:00</span></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td class="hour" rowspan="4"><span>10:00</span></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td class="hour" rowspan="4"><span>11:00</span></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td class="hour" rowspan="4"><span>12:00</span></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td class="hour" rowspan="4"><span>1:00</span></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td class="hour" rowspan="4"><span>2:00</span></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    
-    <tr>
-      <td class="hour" rowspan="4"><span>3:00</span></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td class="hour" rowspan="4"><span>4:00</span></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td class="hour" rowspan="4"><span>5:00</span></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td class="hour" rowspan="4"><span>6:00</span></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td class="hour" rowspan="4"><span>7:00</span></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td class="hour" rowspan="4"><span>8:00</span></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-  </tbody>
-</table>
-</head>
+   },
+   selectable: true,
+   selectHelper: true,
+   select: function(start, end, allDay) {
+   var title = prompt('Event Title:');
 
+   if (title) {
+   var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
+   var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+   $.ajax({
+	   url: 'add_events.php',
+	   data: 'title='+ title+'&start='+ start +'&end='+ end,
+	   type: "POST",
+	   success: function(json) {
+	   alert('Added Successfully');
+	   }
+   });
+   calendar.fullCalendar('renderEvent',
+   {
+	   title: title,
+	   start: start,
+	   end: end,
+	   allDay: allDay
+   },
+   true
+   );
+   }
+   calendar.fullCalendar('unselect');
+   },
+
+   editable: true,
+   eventDrop: function(event, delta) {
+   var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+   var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+   $.ajax({
+	   url: 'update_events.php',
+	   data: 'title='+ event.title+'&start='+ start +'&end='+ end +'&id='+ event.id ,
+	   type: "POST",
+	   success: function(json) {
+	    alert("Updated Successfully");
+	   }
+   });
+   },
+   eventClick: function(event) {
+	var decision = confirm("Do you really want to do that?"); 
+	if (decision) {
+	$.ajax({
+		type: "POST",
+		url: "delete_events.php",
+		data: "&id=" + event.id,
+		 success: function(json) {
+			 $('#calendar').fullCalendar('removeEvents', event.id);
+			  alert("Updated Successfully");}
+	});
+	}
+  	},
+   eventResize: function(event) {
+	   var start = $.fullCalendar.formatDate(event.start, "yyyy-MM-dd HH:mm:ss");
+	   var end = $.fullCalendar.formatDate(event.end, "yyyy-MM-dd HH:mm:ss");
+	   $.ajax({
+	    url: 'update_events.php',
+	    data: 'title='+ event.title+'&start='+ start +'&end='+ end +'&id='+ event.id ,
+	    type: "POST",
+	    success: function(json) {
+	     alert("Updated Successfully");
+	    }
+	   });
+	}
+   
+  });
+  
+ });
+
+</script>
+<style>
+ body {
+  margin-top: 40px;
+  text-align: center;
+  font-size: 14px;
+  font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
+  }
+ #calendar {
+  width: 850px;
+  margin: 0 auto;
+  }
+</style>
+</head>
+<body>
+ 
+ <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+         <div class="navbar-header">
+          <a href="#"><img src="img/title.png"></img></a>
+         </div>
+           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="calendar.php">Home</a></li>
+                <li><a href="inputtime.php">Schedule</a></li>
+                <li><a href="ta-list.php">Notify</a></li>
+                <li style="float:right"> <a href="logout.php">Logout</a></li>
+               </ul>     
+           </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+ </nav>
+
+ <div id='calendar'></div>
+</body>
 </html>
