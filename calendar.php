ad31bf5490be4639b3b1b3569f15d2d575b6485c
Kush Mahajani <kmahajani@scu.edu>
2017-11-14 16:45:38 +0000

    Calendar works better

--- calendar.php
+++ calendar.php
@@ -1,22 +1,29 @@
 <?php
-
+ require('auth.php');
+ require('dbconn.php');
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="css/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="calendar-style.css">
+ <link rel="stylesheet" type="text/css" href="legend-style.css">
+ <link rel="stylesheet" type="text/css" href="login-style.css">
+<meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="css/hover.css">
-<link rel="stylesheet" href="css/style.css">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> <!--<<check-->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 <script src="js/lib/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
-<link rel="stylesheet" type="text/css" href="calendar-style.css"> 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>  <!--<<check-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+
 <script>
 
  $(document).ready(function() {
@@ -33,7 +40,7 @@
     right: 'month,agendaWeek,agendaDay'
    },
 
-   events: "events.php",
+   events: "/events.php",
 
    eventRender: function(event, element, view) {
     if (event.allDay === 'true') {
@@ -42,31 +49,31 @@
      event.allDay = false;
     }
    },
-   selectable: true,
-   selectHelper: true,
+   //selectable: true,
+   //selectHelper: true,
    select: function(start, end, allDay) {
    var title = prompt('Event Title:');
 
    if (title) {
-   var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
-   var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
-   $.ajax({
-	   url: 'add_events.php',
-	   data: 'title='+ title+'&start='+ start +'&end='+ end,
-	   type: "POST",
-	   success: function(json) {
-	   alert('Added Successfully');
-	   }
-   });
-   calendar.fullCalendar('renderEvent',
-   {
-	   title: title,
-	   start: start,
-	   end: end,
-	   allDay: allDay
-   },
-   true
-   );
+    var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
+    var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+    $.ajax({
+	    url: 'add_events.php',
+	    data: 'title='+ title+'&start='+ start +'&end='+ end,
+	    type: "POST",
+	    success: function(json) {
+	     alert('Added Successfully');
+	    }
+    });
+    calendar.fullCalendar('renderEvent',
+    {
+	    title: title,
+	    start: start,
+	    end: end,
+	    allDay: allDay
+    },
+    true
+    );
    }
    calendar.fullCalendar('unselect');
    },
@@ -113,39 +120,36 @@
   });
   
  });
-
 </script>
-<style>
- body {
-  margin-top: 40px;
-  text-align: center;
-  font-size: 14px;
-  font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
-  }
- #calendar {
-  width: 850px;
-  margin: 0 auto;
-  }
-</style>
+	<nav class="navbar navbar-inverse">
+	  <div class="container-fluid" style="width:100%">
+	    <div class="navbar-header">
+	      <a class="navbar-brand glyphicon glyphicon-home" href="calendar.php">Ta</a>
+	    </div>
+	    <ul class="nav navbar-nav" style="display:inline">
+	      <li><a href="inputtime.php">Schedule</a></li>
+	      <li><a href="ta-list.php">Notify</a></li>
+	    </ul>
+	    <ul class="nav navbar-nav navbar-right" style= "display:inline">
+	      <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+	       <?php echo('<li><a href="login.php"> <span class="glyphicon glyphicon-user"></span>'.$_SESSION['username'].'</a></li>'); ?>
+	       <!- if time should i add modal? ...that when you click on username it display status ta or prof?--->
+	    </ul>
+	  </div>
+	</nav>
 </head>
-<body>
- 
- <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-    <div class="container-fluid">
-         <div class="navbar-header">
-          <a href="#"><img src="img/title.png"></img></a>
-         </div>
-           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul class="nav navbar-nav navbar-right">
-                <li><a href="calendar.php">Home</a></li>
-                <li><a href="inputtime.php">Schedule</a></li>
-                <li><a href="ta-list.php">Notify</a></li>
-                <li style="float:right"> <a href="logout.php">Logout</a></li>
-               </ul>     
-           </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
- </nav>
 
- <div id='calendar'></div>
-</body>
+
+<body>
+ <div class = 'cont' >
+  <div id='calendar'></div>
+    <div class='my-legend'>
+   <div class='legend-title'>Section Times</div>
+   <div class='legend-scale'>
+    <ul class='section-times'>
+     <li><span style='background:#B30838;'></span>9:15-12:00</li>
+     <li><span style='background:#FEBE10;'></span>2:15-5:00</li>
+     <li><span style='background:#A7A586;'></span>5:15-8:00</li>
+     <li><span style='background:#FB8072;'></span>Other</li>
+    </ul></div></div></body>
 </html>
