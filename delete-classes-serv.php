80d9e6d13292455ec1909b0bce584bceae95abe0
Kush Mahajani <kmahajani@scu.edu>
2017-11-14 17:37:39 +0000

    add class deletion

---
+++ delete-classes-serv.php
@@ -0,0 +1,27 @@
+<?php
+	require('dbconn.php');
+	require('errors.php');
+
+	$errors = array(); 
+
+	// REGISTER USER
+	if (isset($_POST['delete_class'])) {
+		// receive all input values from the form
+		$section_id = mysqli_real_escape_string($db, $_POST['section_id']);
+		$date = mysqli_real_escape_string($db, $_POST['date']);
+		$starttime = mysqli_real_escape_string($db, $_POST['starttime']);
+	
+		// form validation: ensure that the form is correctly filled
+		if (empty($section_id)) { array_push($errors, "Section ID is required"); }
+		if (empty($date)) { array_push($errors, "Date is required"); }
+		if (empty($starttime)) { array_push($errors, "Start time is required"); }
+
+		// add class if there are no errors in the form
+		if (count($errors) == 0) {
+			$query = "DELETE FROM CLASSES
+			            WHERE section_id = $section_id AND section_date = '$date' AND start_time = '$starttime'";
+			echo($query);
+			mysqli_query($db, $query);
+		}
+	}
+?>
---
+++ delete-classes.php
@@ -0,0 +1,37 @@
+<?php
+	require('auth.php');
+	if ($_SESSION['role'] != "prof") {
+		header("HTTP/1.1 403 Forbidden" );
+		exit;
+	}
+	require('delete-classes-serv.php');
+	$errors = array();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Delete Classes</title>
+		<link rel="stylesheet" type="text/css" href="login-style.css">
+	</head>
+	<body>
+		<div class="header">
+			<h2>Delete Classes</h2>
+		</div>
+		<form action="delete-classes.php" method="post">
+			<?php include('errors.php'); ?>
+			<div class="input-group">
+				<label>Section ID</label>
+				<input type="text" name="section_id">
+			</div>
+			<div class="input-group">
+				<label>Date</label>
+				<input type="date" name="date">
+			</div>
+			<div class="input-group">
+				<label>Start Time</label>
+				<input type="text" name="starttime">
+			</div>
+			<div>
+				<input type="submit" class="btn" name="delete_class" value="Delete Class">
+			</div>
+	</body>
