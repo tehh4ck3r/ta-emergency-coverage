272fa3c6cee104eb05c0e956ccb7fafd03d2c65f
Kush Mahajani <kmahajani@scu.edu>
2017-11-22 18:02:07 -0700

    fix tags and tabs

--- modify-classes.php
+++ modify-classes.php
@@ -17,33 +17,40 @@
 		<div class="header">
 			<h2>Add Classes</h2>
 		</div>
+		
 		<form action="modify-classes.php" method="post">
 			<?php include('errors.php'); ?>
 			<div class="input-group">
 				<label>Department</label>
 				<input type="text" name="department"> 
 			</div>
+			
 			<div class="input-group">
 				<label>Catalog Number</label>
 				<input type="text" name="catalognum">
 			</div>
+			
 			<div class="input-group">
 				<label>Section ID</label>
 				<input type="text" name="section_id">
 			</div>
+			
 			<div class="input-group">
 				<label>Date</label>
 				<input type="date" name="date">
 			</div>
+			
 			<div class="input-group">
 				<label>Start Time</label>
 				<input type="text" name="starttime">
 			</div>
+			
 			<div class="input-group">
 				<label>End Time</label>
 				<input type="text" name="endtime">
 			</div>
-				<div class="input-group">
+			
+			<div class="input-group">
 				<label>TA</label>
 				<select name="ta">
 					<?php
@@ -56,7 +63,10 @@
 					?>
 				</select>
 			</div>
+			
 			<div>
 				<input type="submit" class="btn" name="add_class" value="Add Class">
 			</div>
+		</form>
 	</body>
+</html>
