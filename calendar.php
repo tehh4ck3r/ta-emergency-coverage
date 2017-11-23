33a5d03d652ff41fd0a71e3ab3be9dafd1f5acba
Kush Mahajani <kmahajani@scu.edu>
2017-11-23 20:22:23 +0000

    moved navbar to its own file

--- calendar.php
+++ calendar.php
@@ -25,28 +25,7 @@
 		<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" /> -->
 		<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script> -->
 
-		<nav class="navbar navbar-inverse">
-			<div class="container-fluid" style="width:100%">
-				<div class="navbar-header">
-					<a class="navbar-brand" href="calendar.php"><span class="glyphicon glyphicon-home"></span></a>
-				</div>
-				<ul class="nav navbar-nav" style="display:inline">
-					<li><a href="inputtime.php">Edit Availability</a></li>
-					<li><a href="ta-list.php">Find Replacements</a></li>
-					<?php 
-						if ($_SESSION['role'] == 'prof') {
-							echo('<li><a href="modify-classes.php">Add Classes</a></li>');
-							echo('<li><a href="delete-classes.php">Remove Classes</a></li>');
-						}
-					?>
-				</ul>
-				<ul class="nav navbar-nav navbar-right" style= "display:inline">
-					<li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
-					<?php echo('<li><a> <span class="glyphicon glyphicon-user" ></span> '.$_SESSION['username'].'</a></li>'); ?>
-					<!-- if time should i add modal? ...that when you click on username it display status ta or prof? -->
-				</ul>
-			</div>
-		</nav>
+		<?php require('navbar.php');?>
 
 		<title>TA Emergency Coverage System</title>
 	</head>
---
+++ navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-inverse">
+	<div class="container-fluid" style="width:100%">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="calendar.php"><span class="glyphicon glyphicon-home"></span></a>
+		</div>
+		<ul class="nav navbar-nav" style="display:inline">
+		<?php 
+				if ($_SESSION['role'] == 'prof') {
+					echo('<li><a href="ta-list.php">View All Teaching Assistants</a></li>');
+					echo('<li><a href="modify-classes.php">Add Classes</a></li>');
+					echo('<li><a href="delete-classes.php">Remove Classes</a></li>');
+				}
+				else {
+					echo('<li><a href="inputtime.php">Edit Availability</a></li>');
+					echo('<li><a href="ta-list.php">Find Replacements</a></li>');
+				}
+			?>
+		</ul>
+		<ul class="nav navbar-nav navbar-right" style= "display:inline">
+			<li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+			<?php echo('<li><a> <span class="glyphicon glyphicon-user" ></span> '.$_SESSION['username'].'</a></li>'); ?>
+			<!-- if time should i add modal? ...that when you click on username it display status ta or prof? -->
+		</ul>
+	</div>
+</nav>
