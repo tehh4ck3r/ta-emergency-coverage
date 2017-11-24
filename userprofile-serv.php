ee666be8544567a8ce7d431fa836978ee45ee01a
Kush Mahajani <kmahajani@scu.edu>
2017-11-24 16:19:10 -0700

    You can look at your upcoming availability on your profile page

--- userprofile-serv.php
+++ userprofile-serv.php
@@ -1,6 +1,6 @@
 <?php
 	require('dbconn.php');
-	
+
 	$username = $_SESSION['username'];
 	$query = "SELECT class_name, section_date, start_time, end_time FROM CLASSES WHERE ta = ? AND section_date >= CURDATE()";
 	$stmt = $db->stmt_init();
@@ -25,5 +25,31 @@
 		echo('<td>'. $i['end_time']. '</td>');
 		echo("</tr>");
 	}
+	echo("</table>");
+	$stmt->close();
+
+	$query = "SELECT date, avail_start, avail_end FROM STUDENTAVAIL WHERE username = ? AND date >= CURDATE()";
+	$stmt = $db->stmt_init();
+	if (!$stmt->prepare($query)) {
+		die("Faied to prepare statement: ".$query);
+	} else {
+		$stmt->bind_param('s', $username);
+	}
+	if(!$stmt->execute()) {
+		die("Error in statement execution: ".$stmt->error);
+	}
+
+	$results = $stmt->get_result();
+	echo('<h2><u>Your Upcoming Availability</u></h2><br/>');
+	echo ('<table class="table table-striped">');
+	echo ('<tr> <th>Date</th> <th>Start Time</th> <th>End Time</th> </tr>');
+	foreach ($results as $i) {
+		echo('<tr>');
+		echo('<td>'. $i['date'].'</td>');
+		echo('<td>'. $i['avail_start']. '</td>');
+		echo('<td>'. $i['avail_end']. '</td>');
+		echo("</tr>");
+	}
+	echo("</table>");
 	$stmt->close();
 ?>
