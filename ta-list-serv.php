2593ee96ec6b4f33327434ac52e3d4a23b6a66a2
Kush Mahajani <kmahajani@scu.edu>
2017-11-24 16:11:18 -0700

    Move ta-list stuff to server side

---
+++ ta-list-serv.php
@@ -0,0 +1,33 @@
+<?php
+	require('dbconn.php');
+	$query = "SELECT first, last, email, phone, notify from USERS where role = 'ta'";
+
+	$stmt = $db->stmt_init();
+
+	if (!$stmt->prepare($query)) {
+		die("Faied to prepare statement: ".$query);
+	} 
+
+	if(!$stmt->execute()) {
+		die("Error in statement execution: ".$stmt->error);
+	}
+
+	$results = $stmt->get_result();
+
+	echo('<h2><u>List of TAs</u></h2><br/>');
+	echo ('<table class="table table-striped">');
+	echo ('<tr> <th>Name</th> <th>email</th> <th>phone</th> <th>primary-contact</th> </tr>');
+	foreach ($results as $i) {
+		echo('<tr>');
+		echo('<td>'.$i['first'].' '. $i['last'].'</td>');
+		echo('<td>'. $i['email'].'</td>');
+		echo('<td>'. $i['phone']. '</td>');
+		if ($i['notify'] == 'phone') {
+			echo('<td>Phone</td>');
+		} else {
+			echo('<td><a href="mailto:'.$i['email'].'?$subject=Lab TA Replacement">Email</a></td>');
+		}
+		echo("</tr>");
+	}
+	$stmt->close();
+?>
