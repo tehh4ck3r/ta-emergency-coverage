7841a916fc5d0f72308d0c25dba503b3c4bff9b7
Kush Mahajani <kmahajani@scu.edu>
2017-11-24 02:02:45 +0000

    Added ability for TAs to search replacements by date/time availability

---
+++ find-replacement-serv.php
@@ -0,0 +1,60 @@
+<?php
+	require('dbconn.php');
+	require('errors.php');
+	$errors = array(); 
+	
+	if (isset($_POST['replacements'])) {
+		$date = $_POST['date'];
+		$timeinput = mysqli_real_escape_string($db, $_POST['time']);
+		
+		if (empty($date)) { array_push($errors, "Date is required"); }
+		if (empty($timeinput)) { array_push($errors, "Time is required"); }
+		
+		$time = '00:00:00';
+		switch ($timeinput) {
+			case '9to12':
+				$time = '09:15:00';
+				break;
+			case '2to5':
+				$time = '14:15:00';
+				break;
+			case '5to8':
+				$time = '17:15:00';
+				break;
+			case 'allday':
+				$time = 'allday';
+				break;
+		}
+		
+		if (count($errors) == 0) {
+			$query = "SELECT first, last, email, phone, notify FROM USERS NATURAL JOIN STUDENTAVAIL WHERE username != ? AND role = 'ta' AND date = ? AND avail_start = ?";
+			$stmt = $db->stmt_init();
+			if (!$stmt->prepare($query)) {
+				die("Faied to prepare statement: ".$query);
+			} else {
+				$stmt->bind_param('sss', $_SESSION['username'], $date, $time);
+			}
+			
+			if(!$stmt->execute()) {
+				die("Error in statement execution: ".$stmt->error);
+			}
+			
+			$results = $stmt->get_result();
+			echo ('<table class="table table-striped">');
+			echo ('<tr> <th>Name</th> <th>email</th> <th>phone</th> <th>primary-contact</th> </tr>');
+			foreach ($results as $i) {
+				echo('<tr>');
+				echo('<td>'.$i['first'].' '. $i['last'].'</td>');
+				echo('<td>'. $i['email'].'</td>');
+				echo('<td>'. $i['phone']. '</td>');
+				if ($i['notify'] == 'phone') {
+					echo('<td>Phone</td>');
+				} else {
+					echo('<td><a href="mailto:'.$i['email'].'?$subject=Lab TA Replacement">Email</a></td>');
+				}
+				echo("</tr>");
+			}
+			$stmt->close();
+		}
+	}
+?>
---
+++ find-replacement.php
@@ -0,0 +1,48 @@
+<?php
+	require('auth.php');
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<link rel="stylesheet" type="text/css" href="/css/common.css">
+		<link rel="stylesheet" type="text/css" href="/css/navbar.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+		<?php require('navbar.php');?>
+		
+		<title>TA Replacement Notification</title>
+	</head>
+	
+	<body class="body-bg">	
+		<!--The Big Pink-->
+		<div class="header">
+			<h2>Find Replacement Teaching Assistants</h2>
+		</div>
+
+		<!--The content-->
+		<div class="content"><form action="find-replacement.php" method="post">
+			<div class="input-group">
+				<label>Date</label>
+				<input type="date" name="date">
+			</div>
+			<div class="input-group">
+				<label>Time</label>
+				<select name="time">
+					<option value="9to12">09:15-12:00</option>
+					<option value="2to5">02:15-05:00</option>
+					<option value="5to8">05:15-08:00</option>
+				</select>
+			</div>
+			<div class="input-group">
+				<button type="submit" class="btn" name="replacements">Submit</button>
+			</div>
+		</form>
+		<?php require('find-replacement-serv.php'); ?></div>
+	</body>
+</html>
