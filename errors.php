0b276f1df49b961599bd2305714ec8aa33173e61
Kush Mahajani <kmahajani@scu.edu>
2017-11-27 17:52:53 -0800

    Commenting: errors

--- errors.php
+++ errors.php
@@ -1,7 +1,13 @@
-<?php  if (count($errors) > 0) : ?>
+<!-- 
+	/* errors.php: used to display errors on various pages. 
+	 * 
+	 * If there are errors, create an errors div and output each error.
+	 */
+-->
+<?php if (count($errors) > 0) : ?>
 	<div class="error">
 		<?php foreach ($errors as $error) : ?>
 			<p><?php echo $error ?></p>
 		<?php endforeach ?>
 	</div>
-<?php  endif ?>
+<?php endif ?>
