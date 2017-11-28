857e4e784d7d878bfeac0a8118a9352aa23932c9
Kush Mahajani <kmahajani@scu.edu>
2017-11-27 17:26:53 -0800

    Commenting: auth.php

--- auth.php
+++ auth.php
@@ -1,4 +1,9 @@
 <?php
+	/* auth.php: used to ensure users are logged in. 
+	 * 
+	 * Starts a PHP session, then checks if their username, (hashed) password,
+	 * and role are set. If not, redirect them to the login page.
+	 */
 	session_start();
 	if (!isset($_SESSION['username']) && !isset($_SESSION['password']) && !isset($_SESSION['role'])) {
 		header('location: login.php');
